7a9404b539524c8310df8354c69c40fa0a82fd02
KangDroid <hyunwoo286@gmail.com>
2021-01-10 22:57:05 +0900

    CH4

--- src/main/java/com/kangdroid/test/service/posts/PostsService.java
+++ src/main/java/com/kangdroid/test/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.kangdroid.test.service.posts;
 
 import com.kangdroid.test.domain.posts.Posts;
 import com.kangdroid.test.domain.posts.PostsRepository;
+import com.kangdroid.test.web.dto.PostsListResponseDto;
 import com.kangdroid.test.web.dto.PostsResponseDto;
 import com.kangdroid.test.web.dto.PostsSaveRequestDto;
 import com.kangdroid.test.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,18 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("No Articles."));
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/com/kangdroid/test/web/PostsApiController.java
+++ src/main/java/com/kangdroid/test/web/PostsApiController.java
@@ -26,4 +26,10 @@ public class PostsApiController {
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsServce.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsServce.delete(id);
+        return id;
+    }
 }
